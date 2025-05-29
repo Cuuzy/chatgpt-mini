5df6565170a5c0a5667c84b2fa4cd97f80c10ec8
Cuuzy <116467150+Cuuzy@users.noreply.github.com>
2025-05-29 22:49:58 +0700

    Add files via upload

---
+++ chatgpt-mini/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        gray: {
+          850: "#1e1e2e"
+        }
+      }
+    }
+  },
+  plugins: []
+};
